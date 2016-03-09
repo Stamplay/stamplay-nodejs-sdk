a0a0008226ee1dfd150ca5f19ec571f384dbcf62
Cristian Roselli <cristian.roselli@stamplay.com>
2016-03-09 14:39:14 +0100

    Added Stripe

--- test/object.js
+++ test/object.js
@@ -5,6 +5,7 @@ assert.throws(function(){
 	var obj = stamplayObject({apiKey:'apiKey',appId:'appId', version:'v1'})().get()
 })
 var genericObj = stamplayObject({apiKey:'apiKey',appId:'appId', version:'v1'})('generic')
+
 assert.equal(typeof genericObj.base, 'undefined')
 assert.equal(typeof genericObj.get, 'function')
 assert.equal(typeof genericObj.save, 'function')
@@ -15,3 +16,4 @@ assert.equal(typeof genericObj.upVote, 'function')
 assert.equal(typeof genericObj.downVote, 'function')
 assert.equal(typeof genericObj.rate, 'function')
 assert.equal(typeof genericObj.comment, 'function')
+assert.equal(typeof genericObj.push, 'function')
